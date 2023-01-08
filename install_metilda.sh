a068d7bc8a966e512eac4a66377d7b56258d623d
psanjay5 <103163043+psanjay5@users.noreply.github.com>
2023-01-07 16:27:42 -0800

    Update install_metilda.sh

--- install_metilda.sh
+++ install_metilda.sh
@@ -1,9 +1,19 @@
 echo '>> Updating information about packages...' >&2
-sudo apt-get -y update
+ sudo apt-get -y update
 
-echo '>> Installing dependencies...' >&2
-sudo apt-get -y install build-essential checkinstall virtualenv python-dev-is-python3 libpq-dev libfreetype6-dev libxft-dev ffmpeg npm nodejs
+ echo '>> Installing dependencies...' >&2
+ sudo apt-get -y install build-essential checkinstall virtualenv python-dev-is-python3 libpq-dev libfreetype6-dev libxft-dev ffmpeg npm nodejs
 
-echo '>> Creating Virtual ENV...' >&2
-virtualenv "../MeTILDA_reproduce"
+ echo '>> Creating Virtual ENV...' >&2
+ virtualenv "../MeTILDA_reproduce"
 
+ echo '>> Installing NPM...' >&2
+ cd ./frontend
+ npm install
+ cd ..
+ 
+ echo '>> Activating Virtual ENV...' >&2
+ source "./bin/activate"
+
+ echo '>> Installing Django Packages...' >&2
+ pip install -r requirements.txt
